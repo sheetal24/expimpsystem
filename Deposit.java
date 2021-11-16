6cd6fe1705f9d4fb7e54cfa77b030e6623a8732f
Sheetal Samnani <sheetal_jn@hotmail.com>
2021-11-16 11:04:55 +0000

    changes to Deposit.java

--- Deposit.java
+++ Deposit.java
@@ -4,8 +4,11 @@ public class Deposit extends MainAccount
 {
     public int DepositAmount(int dep)
     {
+       //modify
+	// learn git conflict pull push rejection
         money = money + dep;
         System.out.println("Account Balance after deposit is : "+money);
         return (money);
+//changes after stage
     }
 }
